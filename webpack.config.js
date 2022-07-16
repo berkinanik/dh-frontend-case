5b36f082ceb2771843d0e8cca1bf0956c67db6e0
Berkin Anik <berkinanik@gmail.com>
2022-07-16 19:49:12 +0300

    webpack | webpack initial configs | react initials

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "./src/index.html"),
+  }),
+];
+
+module.exports = {
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+  entry: path.resolve(__dirname, "./src/index.tsx"),
+  mode: mode,
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+};
