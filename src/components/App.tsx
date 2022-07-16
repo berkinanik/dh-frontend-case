07abf4a595f0b7228b389d911d60dd3f9e190ce7
Berkin Anik <berkinanik@gmail.com>
2022-07-16 20:59:22 +0300

    prettier | eslint-prettier | rules | fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App: React.FC = () => {
-  const [name, setName] = useState('')
-  return <div>
-    <h1 style={{ color: 'red' }}>DH Frontend Case</h1>
-    <br />
-    <input value={name} onChange={e => setName(e.target.value)} />
-  </div>
-}
+  const [name, setName] = useState('');
+  return (
+    <div>
+      <h1 style={{ color: 'red' }}>DH Frontend Case</h1>
+      <br />
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-const isDev = process.env.NODE_ENV !== 'production'
-const mode = process.env.NODE_ENV || 'development'
+const isDev = process.env.NODE_ENV !== 'production';
+const mode = process.env.NODE_ENV || 'development';
 
 const plugins = [
   new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, './src/index.html')
-  })
-]
+    template: path.resolve(__dirname, './src/index.html'),
+  }),
+];
 
 if (isDev) {
-  plugins.push(new ReactRefreshWebpackPlugin())
+  plugins.push(new ReactRefreshWebpackPlugin());
 }
 
 module.exports = {
   devServer: {
     hot: true,
     open: true,
-    port: 3000
+    port: 3000,
   },
   entry: path.resolve(__dirname, './src/index.tsx'),
   mode,
@@ -32,23 +32,23 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              plugins: [isDev && require.resolve('react-refresh/babel')].filter(Boolean)
-            }
-          }
-        ]
-      }
-    ]
+              plugins: [isDev && require.resolve('react-refresh/babel')].filter(Boolean),
+            },
+          },
+        ],
+      },
+    ],
   },
   output: {
     path: path.resolve(__dirname, './build'),
     filename: 'bundle.js',
-    clean: true
+    clean: true,
   },
   plugins,
   resolve: {
     alias: {
-      components: path.resolve(__dirname, './src/components')
+      components: path.resolve(__dirname, './src/components'),
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  }
-}
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+};
