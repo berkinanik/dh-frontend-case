10507ba5c6bfbd90854aa7913407494af98d5c7c
Berkin Anik <berkinanik@gmail.com>
2022-07-18 12:47:10 +0300

    restaurant details page responsive styles

--- src/components/RestaurantHeader/index.tsx
+++ src/components/RestaurantHeader/index.tsx
@@ -28,24 +28,26 @@ export const RestaurantHeader: React.FC<RestaurantHeaderProps> = ({
 }) => {
   return (
     <header className={styles.container}>
-      <div className={styles.details__container}>
+      <div className={styles.details}>
         <img className={styles.details__logo} alt="Restaurant Logo" src="images/restaurant-logo.png" />
-        <div className={styles.details}>
-          <h1 className={styles.details__name}>{name + ' (' + address + ')'}</h1>
-          <div className={styles.details__points}>
-            <Badge mode="default" icon="icons/super-delivery.svg" />
-            <Badge mode="point" value={speed.toFixed(1).replace('.', ',')} badgeText="Hız" />
-            <Badge mode="point" value={serving.toFixed(1).replace('.', ',')} badgeText="Servis" />
-            <Badge mode="point" value={flavour.toFixed(1).replace('.', ',')} badgeText="Lezzet" />
-          </div>
-          <div className={styles.details__delivery}>
-            <Badge mode="info" value={formatMoney(minimumPrice)} badgeText="Min. Tutar" icon="icons/min-amount.svg" />
-            <Badge
-              mode="info"
-              value={deliveryTime.toFixed(2).replace('.', ',') + ' dk.'}
-              badgeText="Servis Süresi"
-              icon="icons/delivery.svg"
-            />
+        <div className={styles.details__info}>
+          <h1 className={styles.details__info__name}>{name + ' (' + address + ')'}</h1>
+          <div className={styles.details__info__badges}>
+            <div className={styles.details__info__badges__points}>
+              <Badge mode="default" icon="icons/super-delivery.svg" />
+              <Badge mode="point" value={speed.toFixed(1).replace('.', ',')} badgeText="Hız" />
+              <Badge mode="point" value={serving.toFixed(1).replace('.', ',')} badgeText="Servis" />
+              <Badge mode="point" value={flavour.toFixed(1).replace('.', ',')} badgeText="Lezzet" />
+            </div>
+            <div className={styles.details__info__badges__delivery}>
+              <Badge mode="info" value={formatMoney(minimumPrice)} badgeText="Min. Tutar" icon="icons/min-amount.svg" />
+              <Badge
+                mode="info"
+                value={deliveryTime.toFixed(2).replace('.', ',') + ' dk.'}
+                badgeText="Servis Süresi"
+                icon="icons/delivery.svg"
+              />
+            </div>
           </div>
         </div>
       </div>
